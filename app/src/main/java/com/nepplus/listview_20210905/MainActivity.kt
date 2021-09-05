6cdc1a4f5c2330032ece5a1a1a492bad87084a98
김다은 <softeun92@naver.com>
2021-09-05 14:57:03 +0900

    목록에서 학생 삭제하기 (LongClick)   -  목록변수.remove(지울데이터)   - 어댑터변수.notifyDataSetChange()

--- app/src/main/java/com/nepplus/listview_20210905/MainActivity.kt
+++ app/src/main/java/com/nepplus/listview_20210905/MainActivity.kt
@@ -65,12 +65,17 @@ class MainActivity : AppCompatActivity() {
 //         리스트뷰의 아이템이 길게 늘렸을때.
         studentListView.setOnItemLongClickListener { adapterView, view, position, l ->
 
-//            ~~ 가 길게 눌림. 토스트 출력
+//            길게 눌린 학생-  > 목록(mStudentlist)에서 제거. => 리스트뷰에서도 빠지게.
+
             val clickedStudent = mStudentList[ position]
 
-            Toast.makeText(this, "${clickedStudent.name}이(가) 길게눌림.", Toast.LENGTH_SHORT).show()
-            )
+//            Toast.makeText(this, "${clickedStudent.name}이(가) 길게눌림.", Toast.LENGTH_SHORT).show()
 
+//            실제로 목록에서 제거시키기
+            mStudentList.remove(clickedStudent)
+
+//            리스트뷰의 어댑터에 -> 변경사항 확인 공지.
+            mAdepter.notifyDataSetChanged()
 
 //             마지막에 결과로 true / false 하나 지정 필요.
             return@setOnItemLongClickListener true
