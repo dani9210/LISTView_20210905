717bfff8de33136538720403764c5962e94d897d
김다은 <softeun92@naver.com>
2021-09-05 12:35:22 +0900

    StudentAdapter를 실제로 쓰려면, 필요한 재료들을 요청.

--- app/src/main/java/com/nepplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20210905/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.nepplus.listview_20210905.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.nepplus.listview_20210905.datas.StudentData
 
-class StudentAdapter : ArrayAdapter<StudentData>() {
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>() {
 }
