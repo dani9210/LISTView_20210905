af1d45be4de59abcb63d28fec58fb99ce2561f42
김다은 <softeun92@naver.com>
2021-09-05 13:38:35 +0900

    row에 tempRow!!를 담아서 결과로 지정

--- app/src/main/java/com/nepplus/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20210905/adapters/StudentAdapter.kt
@@ -26,6 +26,13 @@ class StudentAdapter(
         if (tempRow == null) {
             tempRow = mInflater.inflate(R.layout.student_list_item, null)
         }
+
+//         tempRow는 절대 null일 가능성이 없다. => 진짜 row 로 옮겨담자.
+        val row = tempRow!!
+
+//        만들어진 row를 getView 작업의 결과물로 지정.
+
+        return row
     }
 
 }
