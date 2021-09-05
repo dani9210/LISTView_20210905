c5c6f643f1a0e673bbe75bee71830bd621e11d40
김다은 <softeun92@naver.com>
2021-09-05 12:30:03 +0900

    어댑터 클래스 생성 + 상속 받기   - ArrayAdapter는 () 를 지원 X. 에러 발생

---
+++ app/src/main/java/com/nepplus/listview_20210905/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.nepplus.listview_20210905.adapters
+
+import android.widget.ArrayAdapter
+import com.nepplus.listview_20210905.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>() {
+}
